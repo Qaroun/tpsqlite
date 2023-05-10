e2bce227acfb381d5c5db171e1bb53ee99645580
achraf <achrafqaroun03@gmail.com>
2023-05-10 17:12:30 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:tpsqlite/AddFilm.dart';
+import 'package:tpsqlite/UpdateFilm.dart';
+import 'package:flutter/material.dart';
+
+import 'Home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Films',
+      home: Home(),
+      routes: {"addfilm": (context) => AddFilm()},
+    );
+  }
+}
